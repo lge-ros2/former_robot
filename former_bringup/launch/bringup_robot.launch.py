f47d2d1712108ec351294d4f8c3432607fde079e
(=YG=) Hyunseok Yang <hyunseok7.yang@lge.com>
2023-06-07 13:27:26 +0900

    modify bringup launch script with namespace

--- former_bringup/launch/bringup_robot.launch.py
+++ former_bringup/launch/bringup_robot.launch.py
@@ -1,28 +1,77 @@
-import os
-
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, GroupAction
-from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-from launch_ros.substitutions import FindPackageShare
-from launch.event_handlers import OnExecutionComplete
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
 from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, Command
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node, PushRosNamespace
+from launch_ros.substitutions import FindPackageShare
+from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
-    # ld = LaunchDescription()
-    use_sim_time = DeclareLaunchArgument("use_sim_time", default_value="false")
+
+    use_namespace = LaunchConfiguration("use_namespace")
+    namespace = LaunchConfiguration("namespace")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+
+    declare_use_namespace_cmd = DeclareLaunchArgument(
+        'use_namespace',
+        default_value='false',
+        description='Whether to apply a namespace to the navigation stack')
+
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='Top-level namespace')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use simulation clock if true')
+
+    remappings = [('/tf', 'tf'),
+                  ('/tf_static', 'tf_static'),
+                  ('/diagnostics', 'diagnostics')]
+
+    output = {"stdout": "screen",
+              "stderr": "screen"}
 
     upload_robot = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
-            get_package_share_directory('former_description'),
+            FindPackageShare('former_description'),
             '/launch/upload_robot.launch.py']
         ),
         launch_arguments = {
-            'use_gazebo_sim': 'false'
+            'use_gazebo_sim': 'false',
+            'namespace': namespace
         }.items()
     )
+
+    robot_localization_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
+        output='screen',
+        remappings=remappings,
+        parameters=[
+            PathJoinSubstitution([
+                FindPackageShare('former_bringup'),
+                'config/ekf.yaml'
+            ]),
+            {'use_sim_time': use_sim_time}
+        ],
+    )
+
+    robot_controllers = PathJoinSubstitution([FindPackageShare('former_bringup'), "config", "controllers_former.yaml"])
+
+    param_substitutions = {}
+
+    configured_robot_controllers_param = RewrittenYaml(
+        source_file=robot_controllers,
+        root_key=namespace,
+        param_rewrites=param_substitutions,
+        convert_types=True)
+
 
     robot_description_content = Command([
         'xacro ',
@@ -30,67 +79,47 @@ def generate_launch_description():
             FindPackageShare('former_description'),
             'urdf/robot.urdf.xacro',
         ]),
-        ' use_gazebo_sim:=', 'false'
+        ' use_gazebo_sim:=', 'false',
+        ' namespace:=', namespace
     ])
-
-    robot_description = {"robot_description": robot_description_content}
-
-    robot_localization_node = Node(
-        package='robot_localization',
-        executable='ekf_node',
-        name='ekf_filter_node',
-        output='screen',
-        parameters=[
-            PathJoinSubstitution([
-                FindPackageShare('former_bringup'),
-                'config/ekf.yaml'
-            ]),
-            {'use_sim_time': LaunchConfiguration('use_sim_time')}
-        ],
-    )
-
-    robot_controllers = PathJoinSubstitution([
-            FindPackageShare('former_bringup'),
-            "config",
-            "controllers_former.yaml"
-        ]
-    )
 
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[
-            robot_description,
-            robot_controllers
-            ],
-        output={
-            "stdout": "screen",
-            "stderr": "screen",
-        },
+        output=output,
+        remappings=remappings,
         respawn=True,
+        parameters=[
+            {"robot_description": robot_description_content},
+            configured_robot_controllers_param
+            ],
     )
 
     load_joint_state_broadcaster = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        remappings=remappings,
+        arguments=["joint_state_broadcaster", "--controller-manager", "controller_manager"],
     )
 
     load_base_controller = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["base_controller", "--controller-manager", "/controller_manager"],
+        remappings=remappings,
+        arguments=["base_controller", "--controller-manager", "controller_manager"],
     )
 
     load_former_io_controller = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["former_io_controller", "--controller-manager", "/controller_manager"],
+        remappings=remappings,
+        arguments=["former_io_controller", "--controller-manager", "controller_manager"],
     )
 
     lidar_bringup = Node(
         package="sick_scan",
         executable="sick_generic_caller",
+        output=output,
         respawn=True,
         arguments=[
             PathJoinSubstitution([
@@ -105,65 +134,53 @@ def generate_launch_description():
             'range_min:=0.05',
             'sw_pll_only_publish:=false',
         ],
-        output={
-            "stdout": "screen",
-            "stderr": "screen",
-        },
-        remappings=[
-            ('front_lidar/scan', 'scan')
-        ]
+        remappings=remappings + [('front_lidar/scan', 'scan')]
     )
 
     auto_docking_bringup = Node(
         package="former_auto_docking",
         executable="auto_docking_node",
+        output=output,
         respawn=True,
         parameters=[
             {"distance_approach": 0.260},
         ],
-        remappings=[
-            ('odom', 'base_controller/odom'),
-            ('cmd_vel', 'base_controller/cmd_vel_unstamped')
-        ],
-        output={
-            "stdout": "screen",
-            "stderr": "screen",
-        }
+        remappings=remappings +
+            [('odom', 'base_controller/odom'),
+            ('cmd_vel', 'base_controller/cmd_vel_unstamped')]
+        ,
     )
 
     gpio_board_bringup = Node(
         package="former_gpio_board",
         executable="main_node",
+        output=output,
         respawn=True,
+        remappings=remappings,
         parameters=[
             {"port_name": "/dev/ttyARDUINO"},
             {"baudrate": 115200},
         ],
-        output={
-            "stdout": "screen",
-            "stderr": "screen",
-        }
     )
 
     imu_bringup = Node(
         package="imu_xg6000_ros2",
         executable="main_node",
+        output=output,
         respawn=True,
+        remappings=remappings,
         parameters=[
             {'port_name': '/dev/ttyIMU'},
             {'baudrate': 38400},
             {'frame_id': 'imu_link'},
         ],
-        output={
-            "stdout": "screen",
-            "stderr": "screen",
-        }
     )
 
     joy_node = Node(
         package='joy',
         executable='joy_node',
         name='joy_node',
+        remappings=remappings,
         parameters=[{
             'dev': '/dev/js0',
             'deadzone': 0.3,
@@ -171,33 +188,46 @@ def generate_launch_description():
         }],
     )
 
+    joy_param = PathJoinSubstitution([FindPackageShare('former_bringup'), 'config/ps5.config.yaml']),
+
+    configured_joy_param = RewrittenYaml(
+        source_file=joy_param,
+        root_key=namespace,
+        param_rewrites=param_substitutions,
+        convert_types=True)
+
     teleop_joy_node = Node(
         package='teleop_twist_joy',
         executable='teleop_node',
         name='teleop_twist_joy_node',
         parameters=[
-            PathJoinSubstitution([
-                FindPackageShare('former_bringup'),
-                'config/ps5.config.yaml'
-            ]),
+            configured_joy_param
         ],
-        remappings=[
-            ('cmd_vel', 'base_controller/cmd_vel_unstamped')
-        ]
+        remappings= remappings +
+            [('cmd_vel', 'base_controller/cmd_vel_unstamped')]
     )
 
-    return LaunchDescription([
-        use_sim_time,
+    bringup_cmd_group = GroupAction([
+        PushRosNamespace(
+            condition=IfCondition(use_namespace),
+            namespace=namespace),
         upload_robot,
         control_node,
         load_joint_state_broadcaster,
         load_base_controller,
         load_former_io_controller,
-        # # robot_localization_node,
+        # robot_localization_node,
         lidar_bringup,
         imu_bringup,
         gpio_board_bringup,
         auto_docking_bringup,
         joy_node,
         teleop_joy_node
+    ])
+
+    return LaunchDescription([
+        declare_use_namespace_cmd,
+        declare_namespace_cmd,
+        declare_use_sim_time_cmd,
+        bringup_cmd_group
     ])
